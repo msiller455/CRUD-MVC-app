f951ddb00f4cf5f839161070a0713a5dba30d852
Michael <msiller455@gmail.com>
2019-01-16 15:21:38 -0800

    made guitar model

---
+++ models/guitars.js
@@ -0,0 +1,34 @@
+const guitars = [
+    {
+        brand: "Fender",
+        build: "Statocaster",
+        color: "Red"
+    },
+    {
+        brand: "Fender",
+        build: "Telecaster",
+        color: "White"
+    },
+    {
+        brand: "Gibson",
+        build: "SG",
+        color: "Black"
+    },
+    {
+        brand: "Gibson",
+        build: "Les Paul",
+        color: "Yellow"
+    },
+    {
+        brand: "PRS",
+        build: "Custom",
+        color: "Green"
+    },
+    {
+        brand: "PRS",
+        build: "McCarty",
+        color: "Orange"
+    }
+]
+
+module.exports = guitars;
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const app = express();
 
+const Guitars = require('./models/guitars');
+
 const port = 8000;
